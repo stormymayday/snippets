f8d0520a6f61066622ef04c7ddaa2d7765895503
stormymayday <stormymayday7@gmail.com>
2024-09-29 23:33:45 -0700

    Deleting a Snippet

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { db } from "@/db";
 import { notFound } from "next/navigation";
+import Link from "next/link";
+import { deleteSnippet } from "@/actions";
 
 interface SnippetDetailsPageProps {
     params: {
@@ -18,11 +20,33 @@ async function SnippetDetailsPage(props: SnippetDetailsPageProps) {
         return notFound();
     }
 
+    const deleteSnippetAction = deleteSnippet.bind(null, snippet?.id);
+
     return (
-        <div>
-            <p>{snippet?.title}</p>
-            <p>{snippet?.code}</p>
-        </div>
+        <>
+            <div className="flex justify-between items-center">
+                <h2>{snippet.title}</h2>
+                <div className="flex gap-2">
+                    <Link
+                        className="bg-blue-500 p-2 px-4 rounded font-bold text-sm text-white"
+                        href={`/${snippet.id}/edit`}
+                    >
+                        edit
+                    </Link>
+                    <form action={deleteSnippetAction}>
+                        <button
+                            className="bg-red-500 p-2 rounded font-bold text-sm text-white"
+                            type="submit"
+                        >
+                            delete
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div>
+                <p>{snippet.code}</p>
+            </div>
+        </>
     );
 }
 export default SnippetDetailsPage;
