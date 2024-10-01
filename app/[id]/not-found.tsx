d094efc00fb3c7e5c49d7e69a647ea22572027bf
stormymayday <stormymayday7@gmail.com>
2024-09-30 18:58:54 -0700

    Styled SnippetNotFoundPage

--- app/[id]/not-found.tsx
+++ app/[id]/not-found.tsx
@@ -1,4 +1,10 @@
 function SnippetNotFoundPage() {
-    return <div>SnippetNotFoundPage</div>;
+    return (
+        <section className="h-[75vh] flex items-center justify-center">
+            <h1 className="font-bold text-3xl text-center">
+                Sorry, but we could not find that snippet
+            </h1>
+        </section>
+    );
 }
 export default SnippetNotFoundPage;
