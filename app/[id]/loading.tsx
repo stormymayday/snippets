f4fcdbc0a1a753849c7737dd17e06ab5f3bf4e1d
stormymayday <stormymayday7@gmail.com>
2024-09-30 19:00:49 -0700

    Styled the SnippetLoadingPage

--- app/[id]/loading.tsx
+++ app/[id]/loading.tsx
@@ -1,4 +1,10 @@
 function SnippetLoadingPage() {
-    return <div>Loading the snippet...</div>;
+    return (
+        <section className="h-[75vh] flex items-center justify-center">
+            <h1 className="font-bold text-3xl text-center">
+                Loading the snippet...
+            </h1>
+        </section>
+    );
 }
 export default SnippetLoadingPage;
