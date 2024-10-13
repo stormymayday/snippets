d66325247db9a62d0bb39df6e48928666cb2671b
stormymayday <stormymayday7@gmail.com>
2024-10-12 19:17:36 -0700

    Wired up the login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,34 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+import { Button } from "@/components/ui/button";
+import LoginButton from "@/components/auth/login-button";
+
+const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["600"],
+});
+
 export default async function LandingPage() {
     return (
         <main className="h-[100vh] flex flex-col items-center justify-center">
             <div className="space-y-6 text-center">
-                <h1 className="text-6xl font-semibold drop-shadow-md">
+                <h1
+                    className={cn(
+                        "text-6xl font-semibold drop-shadow-md",
+                        poppins.className
+                    )}
+                >
                     💻 Snippets
                 </h1>
                 <p className="text-lg">Your Code, Just a Snippet Away!</p>
+                <div>
+                    <LoginButton>
+                        <Button variant="default" size="lg">
+                            Sign in
+                        </Button>
+                    </LoginButton>
+                </div>
             </div>
         </main>
     );
