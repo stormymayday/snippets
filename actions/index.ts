93023f1624645126525c9eb4262d67722afad4cc
stormymayday <stormymayday7@gmail.com>
2024-10-14 20:52:50 -0700

    Fixed the register server action import

--- actions/index.ts
+++ actions/index.ts
@@ -5,7 +5,19 @@ import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
 
 import * as z from "zod";
-import { LoginSchema } from "@/schemas";
+import { LoginSchema, RegisterSchema } from "@/schemas";
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+    const validatedFields = RegisterSchema.safeParse(values);
+
+    if (!validatedFields.success) {
+        return {
+            error: "Invalid fields!",
+        };
+    }
+
+    return { success: "Email sent!" };
+};
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values);
