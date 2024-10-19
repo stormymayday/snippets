d41a370f1b6bdc46b7ab9d6c2fa586ce38f4cd1a
stormymayday <stormymayday7@gmail.com>
2024-10-18 18:21:05 -0700

    Credentials config

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,38 @@
-import GitHub from "next-auth/providers/github";
 import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "@/schemas";
+import { getUserByEmail } from "@/utils/user";
+import bcrypt from "bcrypt";
 
-export default { providers: [GitHub] } satisfies NextAuthConfig;
+export default {
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const validatedFields = LoginSchema.safeParse(credentials);
+
+                if (!validatedFields.success) {
+                    return null;
+                }
+
+                const { email, password } = validatedFields.data;
+
+                const user = await getUserByEmail(email);
+
+                if (!user || !user.password) {
+                    return null;
+                }
+
+                const passwordMatch = await bcrypt.compare(
+                    password,
+                    user.password
+                );
+
+                if (!passwordMatch) {
+                    return null;
+                }
+
+                return user;
+            },
+        }),
+    ],
+} satisfies NextAuthConfig;
