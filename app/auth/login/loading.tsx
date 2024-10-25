b481ca49247ef1a75df1bd4760175ab8d8ab0756
stormymayday <stormymayday7@gmail.com>
2024-10-25 16:45:29 -0700

    Removed the LoginLoadingPage

--- app/auth/login/loading.tsx
+++
@@ -1,8 +0,0 @@
-function LoginLoadingPage() {
-    return (
-        <div className="h-[100vh] flex items-center justify-center">
-            <h1 className="text-xl font-bold">Loading...</h1>
-        </div>
-    );
-}
-export default LoginLoadingPage;
