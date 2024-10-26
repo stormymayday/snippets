613ba19ff2580f0eb2d8c78f7bf5480d8af74ab7
stormymayday <stormymayday7@gmail.com>
2024-10-25 17:38:45 -0700

    Email Verification

--- actions/index.ts
+++ actions/index.ts
@@ -11,8 +11,9 @@ import { getUserByEmail } from "@/utils/user";
 import { signIn } from "@/auth";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { generateVerificationToken } from "@/utils/tokens";
+import { getVerificationTokenByToken } from "@/utils/verification-token";
 import { sendVerificationEmail } from "@/utils/mail";
-import FromSuccess from "@/components/form-success";
+import { error } from "console";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -121,6 +122,48 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
         }
         throw error;
     }
+};
+
+export const newVerification = async (token: string) => {
+    // Fetching existing token from db
+    const existingToken = await getVerificationTokenByToken(token);
+
+    // Checking if token exists
+    if (!existingToken) {
+        return {
+            error: "Token does not exist",
+        };
+    }
+
+    // Checking if token has expired
+    const tokenExpired = new Date(existingToken.expires) < new Date();
+    if (tokenExpired) {
+        return { error: "Token has expired" };
+    }
+
+    // Fetching an existing user
+    const existingUser = await getUserByEmail(existingToken.email);
+
+    // Checking if the user exists
+    if (!existingUser) {
+        return { error: "Email does not exist" };
+    }
+
+    // Updating the user
+    // Setting emailVerified to current date and email to the email from token
+    await db.user.update({
+        where: { id: existingUser.id },
+        data: { emailVerified: new Date(), email: existingToken.email },
+    });
+
+    // Deleting existing token
+    await db.verificationToken.delete({
+        where: {
+            id: existingToken.id,
+        },
+    });
+
+    return { success: "Email verified!" };
 };
 
 export const createSnippet = async (
--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -2,10 +2,38 @@
 
 import CardWrapper from "@/components/auth/card-wrapper";
 import { BeatLoader } from "react-spinners";
+import FormError from "@/components/form-error";
+import FormSuccess from "@/components/form-success";
 import { useSearchParams } from "next/navigation";
+import { newVerification } from "@/actions";
+import { useState, useCallback, useEffect } from "react";
 
 function NewVerificationForm() {
+    const [error, setError] = useState<string | undefined>();
+    const [success, setSuccess] = useState<string | undefined>();
+
     const searchParams = useSearchParams();
+
+    const token = searchParams.get("token");
+
+    const onSubmit = useCallback(async () => {
+        try {
+            if (!token) {
+                setError("Missing token");
+                return;
+            }
+
+            const data = await newVerification(token);
+            setSuccess(data.success);
+            setError(data.error);
+        } catch (error) {
+            setError("Something went wrong!");
+        }
+    }, [token]);
+
+    useEffect(() => {
+        onSubmit();
+    }, [onSubmit]);
 
     return (
         <CardWrapper
@@ -14,7 +42,11 @@ function NewVerificationForm() {
             backButtonHref="/auth/login"
         >
             <div className="flex items-center justify-center">
-                <BeatLoader />
+                {!success && !error && <BeatLoader />}
+
+                <FormSuccess message={success} />
+
+                {!success && <FormError message={error} />}
             </div>
         </CardWrapper>
     );
